62246b76246bed2484c522d3ad497f91a08e08f5
Matt <mattsully01@gmail.com>
2019-07-28 18:34:05 -0600

    comm changes

--- rpg.js
+++ rpg.js
@@ -1,7 +1,16 @@
+// require mysql
 var mysql = require("mysql");
-
 // Load the NPM Package inquirer
 var inquirer = require("inquirer");
+
+
+// global variables for storing item id and quantity for later checks
+var dbItem;
+var dbQuantity;
+var userItem;
+var userQuantity;
+
+
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -15,46 +24,54 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    querryClasses();
+    querryItems();
 
 })
 
-function querryClasses() {
+function querryItems() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | $" + res[i].price + " | " + res[i].stock_quantity + " remaining");
         }
         console.log("-----------------------------------");
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "Which item do you chose?",
+                    name: "id"
+                },
+                {
+                    type:"input",
+                    message: "how many would you like to purchase",
+                    name: 'quantity'
+                },
+                {
+                    type: "confirm",
+                    message: "Are youo sure you want to purchase these items",
+                    name: "confirm",
+                    default: true
+                }
+            ])
+
+            .then(function (inquirerResponse) {
+                if (inquirerResponse.confirm) {
+                    item = inquirerResponse.id
+                    quantity = inquirerResponse.quantity
+                    console.log("\n you've chosen item number " + inquirerResponse.id);
+                    console.log(" Chosen quantity " + inquirerResponse.quantity);
+                }
+                connection.query("SELECT * FROM products")
+
+
+
+
+            })
     })
 }
 
-inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "To select item, Enter the items Id Number",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "How many would you like to purchase?",
-            name: "quantity"
-        },
-        {
-            type: "confirm",
-            message: "Are you sure:",
-            name: "confirm",
-            default: true 
-        }
-    ])
 
-.then(function(inquirerResponse){
-    if (inquirerResponse.confirm){
-        console.log("\n you've chosen item number " + inquirerResponse.id);
-        console.log(" Chosen quantity " + inquirerResponse.quantity);
-    }
-})
 
 
 
