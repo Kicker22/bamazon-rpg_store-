cf2feb4f0ca2e68e147799e26cd64c64f0e18cb9
Matt <mattsully01@gmail.com>
2019-07-29 23:31:04 -0600

    adding conditionals

--- rpg.js
+++ rpg.js
@@ -4,11 +4,10 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 
-// global variables for storing item id and quantity for later checks
+// global variables for storing item_id and stock_quantity for later checks
 var userItem;
 var userQuantity;
-var stockQuantity;
-var dbQuantity; 
+var dbQuantity;
 
 
 
@@ -23,18 +22,23 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
+    // console.log("connected as id " + connection.threadId);
     querryItems();
-
 })
+
 
 function querryItems() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
+        console.log("-------------------------------");
+
+        // this for loop will display all of the items in the shop 
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | $" + res[i].price + " | " + res[i].stock_quantity + " remaining");
         }
         console.log("-----------------------------------");
+
+        // inquirer to prompt user input
         inquirer
             .prompt([
                 {
@@ -49,7 +53,7 @@ function querryItems() {
                 },
                 {
                     type: "confirm",
-                    message: "Are youo sure you want to purchase these items",
+                    message: "Are you sure you want to purchase these items",
                     name: "confirm",
                     default: true
                 }
@@ -65,10 +69,9 @@ function querryItems() {
 
                     console.log("\n you've chosen item number " + userItem);
                     console.log(" Chosen quantity " + userQuantity);
-                    console.log(" Your order has been placed")
-                    console.log("--------------------------------------------------------")
                 } else {
-                    console.log("tansaction aborted")
+                    console.log("Come again soon")
+                    connection.end()
                 }
 
 
@@ -76,20 +79,29 @@ function querryItems() {
     })
 }
 
-function mathThatIsh (){
-    connection.query("SELECT stock_quantity FROM products WHERE item_id =" + userItem ,function(err,res){
-            dbQuantity = res
+function mathThatIsh() {
+    connection.query("SELECT stock_quantity FROM products WHERE item_id =" + userItem, function (err, res) {
+        dbQuantity = res
 
-            for(i = 0; i < dbQuantity.length; i++){
-                connection.query("UPDATE products SET stock_quantity = "+( dbQuantity[i].stock_quantity - userQuantity )+" WHERE item_id =" + userItem)
-             
+        for (i = 0; i < dbQuantity.length; i++) {
+            if(dbQuantity[i].stock_quantity < userQuantity){
+                console.log("Insufficient quantity!")
+                console.log("______________________________")
+            }else{
+                console.log(" Your order has been placed")
+                console.log("--------------------------------------------------------")
+                connection.query("UPDATE products SET stock_quantity = " + (dbQuantity[i].stock_quantity - userQuantity) + " WHERE item_id =" + userItem)
             }
+        }
         connection.end();
-        
-
     })
-
 };
+
+
+
+
+
+
 
 
 
